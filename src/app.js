ad7f7f23ef8c5fb5f86fb691b4cacf9db3224475
Juan E Gonzalez <158267755+juan007-play@users.noreply.github.com>
2024-08-16 02:08:51 +0000

    randomCardGenerator

--- src/app.js
+++ src/app.js
@@ -2,10 +2,40 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const suits = ["♥", "♦", "♠", "♣"];
+  const values = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A"
+  ];
+
+  const randomSuit = suits[Math.floor(Math.random() * suits.length)];
+  const randomValue = values[Math.floor(Math.random() * values.length)];
+
+  const card = document.getElementById("card");
+
+  const suitColor = randomSuit === "♥" || randomSuit === "♦" ? "red" : "black";
+  card.innerHTML = `
+    <div class="corner-icon top-left" style="font-size: 3rem;">
+      <span style="color: ${suitColor};">${randomSuit}</span>
+    </div>
+    <div class="corner-icon bottom-right" style="font-size: 3rem;">
+      <span style="color: ${suitColor};">${randomSuit}</span>
+    </div>
+    <div class="d-flex justify-content-center align-items-center" style="font-size: 5rem;">
+      ${randomValue}
+    </div>
+  `;
 };
